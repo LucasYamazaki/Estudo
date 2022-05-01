408cc5948b7d9693961f07dfcab2090a5d2a3bab
LucasYamazaki <lu.yamazaki@gmail.com>
2022-05-01 16:17:17 -0300

    Gerenciador de tarefas - Back-End

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-palavra = 'perfume'
-digitadas = []
-chances = 5
+atividades = []
+def nova_trefa():
+    nova_tarefa = input("Digite uma tarefa: ").strip()
+    atividades.append(nova_tarefa)
+def remover_tarefa():
+    selecionar = input("Digite o nome da tarefa que quer remover: ").strip()
+    try:
+        atividades.remove(selecionar)
+        print("Atividade removida.")
+    except Exception:
+        print("Atividade não encontrada.")
+
+print("---Menu---")
+print("1-Ver tarefas")
+print("2-Adicionar tarefa")
+print("3-Remover tarefa")
+print("4-Mostrar ultima tarefa")
 while True:
-    letra = input("Digite uma letra: ")
-    if len(letra) > 1:
-        print("Mais de uma letra? Sério?... Digite só uma!")
-        continue
-    digitadas.append(letra)
-
-    if letra in palavra:
-        print(f"BOA!! A letra {letra} tem na palavra")
-
+    comando = int(input("Digite um comando: "))
+    if comando == 1:
+        print(atividades)
+    elif comando == 2:
+        nova_trefa()
+    elif comando == 3:
+        remover_tarefa()
+    elif comando == 4:
+        print(atividades[-1])
+        atividades.pop()
+        print(atividades[-1])
     else:
-        print(f"Pena, a letra {letra} não pertence a palavra")
-        digitadas.pop()
-        chances -= 1
-    if chances <=0:
-        print('Você perdeu!')
-        break
-
-    palavra_temporaria = ''
-    for letra_palavra in palavra:
-        if letra_palavra in digitadas:
-            palavra_temporaria += letra_palavra
-        else:
-            palavra_temporaria += '*'
-
-    if palavra_temporaria == palavra:
-        print('Parabéns!! Você acertou.')
-    else:
-        print(palavra_temporaria)
-        print(f'Chances restantes: {chances}')
+        print("Comando inválido")
