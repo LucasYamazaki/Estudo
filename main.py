b4d4d222993ae05aceae3c405fd3164ab93e757c
LucasYamazaki <lu.yamazaki@gmail.com>
2022-05-06 15:57:39 -0300

    Comando de voz finalizado

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import speech_recognition as sr
+
+rec = sr.Recognizer()
+
 atividades = []
 
 #Setando a lista 'atividades' com o arquivo txt.
@@ -5,46 +9,54 @@ with open('tarefas.txt','r') as arquivo:
     for valor in arquivo:
         atividades.append(valor)
 
+#Adicionar nova tarefa
 def nova_trefa():
     with open('tarefas.txt','a') as arquivo:
-        valor = str(input("Digite uma tarefa: "))
-        arquivo.write(str(valor) + '\n')
-        atividades.append(valor)
+        with sr.Microphone() as mic:
+            rec.adjust_for_ambient_noise(mic)
+            print("Pode falar: ")
+            audio = rec.listen(mic)
+            texto = rec.recognize_google(audio, language= "pt-BR")
+        arquivo.write(str(texto) + '\n')
+        atividades.append(texto)
+
+#Remover tarefa
 def remover_tarefa():
-    selecionar = str(input("Digite o nome da tarefa que quer remover: ")).strip()
+    with sr.Microphone() as mic:
+        rec.adjust_for_ambient_noise(mic)
+        print("Diga a tarefa para remover: ")
+        audio = rec.listen(mic)
+        texto = rec.recognize_google(audio, language= "pt-BR")
     with open('tarefas.txt','w') as arquivo:
-        for valor in atividades:
-            if valor == selecionar:
-                atividades.remove(selecionar)
-                print("Atividade removida")
+        try:
+            atividades.remove(texto)
+            print("Atividade removida")
+            for valor in atividades:
+                arquivo.write(str(valor) + '\n')
+        except Exception:
+                print("Atividade não encontrada")
                 for valor in atividades:
                     arquivo.write(str(valor) + '\n')
-            else:
-                print("Atividade não encontrada")
-#    try:
-#        atividades.remove(selecionar)
-#        print("Atividade removida.")
-#    except Exception:
-#        print("Atividade não encontrada.")
+    
 
+#Menu
 print("---Menu---")
 print("1-Ver tarefas")
 print("2-Adicionar tarefa")
 print("3-Remover tarefa")
-print("4-Mostrar ultima tarefa")
 while True:
-    comando = int(input("Digite um comando: "))
-    if comando == 1:
+    with sr.Microphone() as mic:
+        rec.adjust_for_ambient_noise(mic)
+        print("Diga um comando: ")
+        audio = rec.listen(mic)
+        comando = rec.recognize_google(audio, language="pt-BR")
+    if comando == "Ver tarefas":
         with open('tarefas.txt','a') as arquivo:
             for valor in arquivo:
                 print(valor)
-    elif comando == 2:
+    elif comando == "Adicionar tarefa":
         nova_trefa()
-    elif comando == 3:
+    elif comando == "excluir tarefa":
         remover_tarefa()
-    elif comando == 4:
-        print(atividades[-1])
-        atividades.pop()
-        print(atividades[-1])
     else:
         print("Comando inválido")
